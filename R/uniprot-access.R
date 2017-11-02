56587b6808b15a831641eb5ea725a415aca21e43
Zebulun Arendsee <arendsee@iastate.edu>
2017-11-02 15:39:53 -0500

    Add uniprot downstream ID retriever

--- R/uniprot-access.R
+++ R/uniprot-access.R
@@ -1,48 +1,18 @@
-
-get_uniprot <- function(focal_taxon){
-
+#' Get the uniprot ids downstream of a node
+#'
+#' @param taxid Ancestral node of clade of interest
+#' @param reference_only Should only reference proteomes be considered?
+#' @return A numeric vector of NCBI taxon ids listing all species in this clade
+#' for which Uniprot has a complete proteome.
+#' @export
+uniprot_downstream_ids <- function(taxid, reference_only=FALSE){
+  ref_str <- if(reference_only){
+    'reference:yes'
+  } else {
+    'complete:yes'
+  }
+  url_str <- glue::glue(
+    'http://www.uniprot.org/taxonomy/?query=ancestor:{taxid}+{ref_str}&format=list'
+  )
+  readLines(curl::curl(url_str)) %>% as.integer
 }
-
-
-    # args = parser()
-    #
-    # if args.print_http:
-    #     httplib2.debuglevel = 1
-    #
-    # proteome = 'reference:yes' if args.reference else 'complete:yes'
-    # include = 'yes' if args.include_isoforms else 'no'
-    #
-    # h = httplib2.Http(args.cache)
-    #
-    # par = {'query': 'ancestor:%s+%s' % (args.top_taxid, proteome),
-    #        'format': 'list'}
-    # response, content = h.request(makeurl('taxonomy', par))
-    #
-    # if args.print_http:
-    #     prettyprint_http(response)
-    #
-    # taxids = content.decode().strip().split("\n")
-    #
-    # for taxid in taxids:
-    #     if args.retrieve_sequence or args.retrieve_annotations:
-    #         if args.retrieve_annotations:
-    #             filename = '%s.tab' % taxid
-    #             par = {'query':'organism:%s' % taxid,
-    #                    'format':'tab',
-    #                    'include':include,
-    #                    'columns':','.join(args.retrieve_annotations)}
-    #         elif args.retrieve_sequence:
-    #             filename = '%s.faa' % taxid
-    #             par = {'query':'organism:%s' % taxid,
-    #                    'format':'fasta',
-    #                    'include':include}
-    #         print("Retrieving %s" % filename, file=sys.stderr)
-    #         url = makeurl('uniprot', par)
-    #         response, content = h.request(url)
-    #         if args.print_http:
-    #             print("URL:%s" % url)
-    #             prettyprint_http(response)
-    #         with open(filename, 'w') as f:
-    #             print(content.decode(), file=f)
-    #     else:
-    #         print(taxid)
