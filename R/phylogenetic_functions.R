ade387821b5c0a28ba1dfcec179fceb960538b7f
Zebulun Arendsee <arendsee@iastate.edu>
2017-11-02 17:49:37 -0500

    Add uncle finder

--- R/phylogenetic_functions.R
+++ R/phylogenetic_functions.R
@@ -1,9 +1,37 @@
-backbone <- function(taxid, byname=FALSE){
+#' Get the lineage of a given species
+#'
+#' @param taxid a single NCBI taxon
+#' @param byname Get ancestor names, instead of NCBI ids
+#' @export
+ancestors <- function(taxid, byname=FALSE){
   taxize::classification(taxid, db='ncbi')[[1]]$id 
 }
 
+#' Get the species level cousins of a taxid
+#'
+#' @param taxid a single NCBI taxon
+#' @export
 cousins <- function(taxid){
-  taxize::downstream(backbone(taxid)[-1], downto='species', db='ncbi')
+  taxize::downstream(ancestors(taxid)[-1], downto='species', db='ncbi')
+}
+
+#' Get all uncles of a species
+#'
+#' @param taxid a single NCBI taxon
+#' @export
+uncles <- function(taxid){
+  # FIXME: cannot find root (taxize issue #639)
+  #        so I remove the first index (root)
+  lineage <- ancestors(taxid)[-1]
+  children <- taxize::children(lineage, db='ncbi') %>% {.[-length(.)]}
+  lineage <- lineage[-1]
+  lapply(
+    seq_along(children),
+    function(i) {
+      setdiff(children[[i]]$childtaxa_id, lineage[i]) %>% as.integer
+    }
+  ) %>%
+    magrittr::set_names(names(children))
 }
 
 check_noise <- function(noise){
