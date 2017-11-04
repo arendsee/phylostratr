64541d247f9733e4f00ffa4584e8e1ef78f750d3
Zebulun Arendsee <arendsee@iastate.edu>
2017-11-04 13:36:00 -0500

    Add taxid2name function using rentrez

--- R/uniprot-access.R
+++ R/uniprot-access.R
@@ -121,6 +121,41 @@ uniprot_cousin_genomes <- function(cousins, dir='strata', prefix='ps_', dryrun=F
   }
 }
 
+#' Transform list of taxids to list of taxid summaries
+#'
+#' @param taxids Vector of NCBI taxonomy ids
+#' @param number of records to retrive at a time (the default should be fine)
+#' @export
+taxid2summary <- function(taxids, chunk_size=250){
+  id_summaries <- as.list(taxids) %>% lapply(function(x) NULL)
+  N <- length(taxids)
+  for(i in (0:(N %/% chunk_size))){
+    i <- i*chunk_size + 1
+    j <- min(N, i + chunk_size - 1)
+    # If retrieving only one record, the return type is [fields]
+    if(i == j){
+      id_summaries[[i]] <- rentrez::entrez_summary(id=taxids[i:j], db='taxonomy')
+    # Else the return type is [[fields]]
+    } else {
+      id_summaries[i:j] <- rentrez::entrez_summary(id=taxids[i:j], db='taxonomy')
+    }
+    if(j < N)
+      Sys.sleep(0.3) # so as not to piss off NCBI
+  }
+  names(id_summaries) <- taxids
+  id_summaries
+}
+
+#' Get the scientific names for a list of taxids
+#'
+#' @param taxids A list of NCBI taxonomy ids
+#' @param ... Arguments passed to \code{taxid2summary}
+#' @export
+taxid2name <- function(taxids, ...){
+  id_summaries <- taxid2summary(taxids, ...)
+  vapply(FUN.VALUE=character(1), id_summaries, function(x) x$scientificname)
+}
+
 #' Select representatives for a strata
 #'
 #' @export
