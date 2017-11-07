4ae0c8da351a672695e5d9de321468bb9bd27c8d
Zebulun Arendsee <arendsee@iastate.edu>
2017-11-07 10:12:39 -0600

    Refactor blast handling
     1. make `strata_blast` pass explicit arguments lists to both `run_blast`
     and `make_blast_database`.
    
     2. `run_blast` and `make_blast_database` now both
       * check for existing data, and skip if a file is found
       * return the output filename

--- R/blast.R
+++ R/blast.R
@@ -4,10 +4,19 @@
 #' @param indir The directory in which to find the protein FASTA sequence
 #' (should contain a file '<taxid>.faa')
 #' @param outdir The directory in which the blast database should be stored
-make_blast_database <- function(taxid, indir, outdir){
+#' @return path to blast database (without extension)
+make_blast_database <- function(
+  taxid,
+  indir  = 'uniprot-seqs',
+  outdir = 'blastdb'
+){
   fastafile <- file.path(indir, paste0(taxid, ".faa"))
   out <- file.path(outdir, taxid)
-  system2('makeblastdb', args=c('-dbtype', 'prot', '-in', fastafile, '-title', taxid, '-out', out))
+  if(!file.exists(paste0(out, ".psq"))){
+    message(sprintf("%s: making blast database ...", taxid))
+    system2('makeblastdb', args=c('-dbtype', 'prot', '-in', fastafile, '-title', taxid, '-out', out))
+  }
+  out
 }
 
 #' BLAST query protein FASTA file against a target species 
@@ -18,18 +27,31 @@ make_blast_database <- function(taxid, indir, outdir){
 #' @param blastresult The output TAB-delimited result file
 #' @param nthreads Number of threads
 #' @param seg Whether to mask the query protein sequences
-run_blastp <- function(query_fastafile, target_taxid, blastdb, blastresult, nthreads=1, seg=FALSE){
-  system2(
-    'blastp',
-    stdout=blastresult,
-    args=c(
-      '-db', file.path(blastdb, target_taxid),
-      '-query', query_fastafile,
-      '-outfmt', '"6 qseqid evalue score"',
-      '-num_threads', nthreads,
-      '-seg', if(seg) {'yes'} else {'no'}
+#' @return The path to the output file
+run_blastp <- function(
+  query_fastafile,
+  target_taxid,
+  blastdb     = 'blastdb',
+  blastresult = paste0(target_taxid, ".tab"),
+  nthreads    = 1,
+  seg         = FALSE
+){
+  if(file.exists(blastresult)){
+    message(sprintf("Skipping %s", target_taxid))
+  } else {
+    message(sprintf("%s: blasting ...", target_taxid))
+    system2(
+      'blastp',
+      stdout=blastresult,
+      args=c(
+        '-db', file.path(blastdb, target_taxid),
+        '-query', query_fastafile,
+        '-outfmt', '"6 qseqid evalue score"',
+        '-num_threads', nthreads,
+        '-seg', if(seg) {'yes'} else {'no'}
+      )
     )
-  )
+  }
   blastresult
 }
 
@@ -39,26 +61,20 @@ run_blastp <- function(query_fastafile, target_taxid, blastdb, blastresult, nthr
 #'
 #' @param query The protein FASTA file for the focal species
 #' @param strata Uncle level strata
-#' @param ... Additional arguments passed to \code{run_blastp}
-strata_blast <- function(query, strata, ...){
+#' @param makedb_args Additional arguments passed to \code{make_blast_database} 
+#' @param blast_args Additional arguments passed to \code{run_blastp}
+strata_blast <- function(query, strata, makedb_args=list(), blast_args=list()){
   lapply(
     strata,
     function(uncles){
       taxids <- unname(unlist(uncles))
       vapply(
-        FUN.VALUE=character(1),
+        FUN.VALUE="",
         taxids,
         function(taxid){
-          blastresult <- paste0(taxid, ".tab")
-          if(file.exists(blastresult)){
-            message(sprintf("Skipping %s", taxid))
-          } else {
-            message(sprintf("%s: making blast database ...", taxid))
-            make_blast_database(taxid, 'uniprot-seqs', 'blastdb')
-            message(sprintf("%s: blasting ...", taxid))
-            run_blastp(query, taxid, 'blastdb', blastresult, ...)
-          }
-          blastresult
+          blastdb <- do.call(make_blast_database, args=append(taxid, makedb_args))
+          blast_args <- append(list(query, taxid, blastdb=blastdb), blast_args)
+          do.call(run_blastp, args=blast_args)
         }
       )
     }
