27ed26906b4f01e3d367d4736ccf8747c52217a9
Zebulun Arendsee <arendsee@iastate.edu>
2017-11-16 04:20:58 -0600

    Add prokaryote sample dataset

--- R/phylostratr.R
+++ R/phylostratr.R
@@ -9,6 +9,16 @@ NULL
 #'
 #' This is a stub
 #'
+#' @section prokaryote_sample:
+#'
+#' A selection of bacterial and archeael species which have UniProt reference
+#' proteoms. These species were selected by randomly choosing one
+#' representative from each class (using the \code{uniprot_sample_prokaryotes}
+#' function with random seed 42).
+#'
+#' This sample is used as the root stratum in both the arabidopsis and yeast
+#' vignettes.
+#'
 #' @docType package
 #' @name phylostratr
 NULL
