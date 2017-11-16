c73e7b1e015ace6bc8ce345142072e4ab31beede
Zebulun Arendsee <arendsee@iastate.edu>
2017-11-16 15:20:05 -0600

    Fix obo plots and recast araport11 data as RDS
    For the araport11 data, we can now read it with `readRDS` and assign to
    to a variable, rather than using the awkward `load` which creates a
    variable for us.

--- R/plot.R
+++ R/plot.R
@@ -8,12 +8,21 @@ eval_bins <- function(evalue){
   bin
 }
 
+#' Plot the max hit scores against each species for one gene
+#'
+#' This is an internal method, a user would normally use \code{plot_obo} or
+#' \code{make_obo_pdf}.
+#'
+#' @param stats A data.frame with the columns [index, score, eval_bins]
+#' @param qseqid The query gene ID
+#' @param xlines A table specifying where the phylostrata lines should be drawn
+#' @return ggplot2 object
 plot_one_obo <- function(stats, qseqid, xlines){
   stats$index = 1:nrow(stats)
   ggplot2::ggplot(stats) +
-    ggplot2::geom_point(ggplot2::aes(x=.data$index, y=.data$score, color=eval_bins), size=0.5) +
+    ggplot2::geom_point(ggplot2::aes_string(x='index', y='score', color='eval_bins'), size=0.5) +
     ggplot2::scale_color_manual(values=c('darkred', 'red', 'orange', 'green', 'blue')) +
-    ggplot2::geom_vline(data=xlines, ggplot2::aes(xintercept=xlines), alpha=0.4) +
+    ggplot2::geom_vline(data=xlines, ggplot2::aes_string(xintercept='xlines'), alpha=0.4) +
     ggplot2::xlim(1, nrow(stats)) +
     ggplot2::theme_minimal() +
     ggplot2::ggtitle(qseqid) +
@@ -26,6 +35,13 @@ plot_one_obo <- function(stats, qseqid, xlines){
     )
 }
 
+#' Plot the max hit scores against each species
+#'
+#' @param d data.frame of the best hits of focal genes against subject species.
+#' It must have the columns [staxid, qseqid, evalue, score, mrca, ps]
+#' @param lower.bound A lower bound at which to truncate scores
+#' @param upper.bound An upper bound at which to truncate scores
+#' @return A named list of ggplot2 objects, with names being the qseqid
 plot_obo <- function(d, lower.bound=0, upper.bound=100){
 
   d <- dplyr::arrange(d, -.data$ps, .data$staxid)
@@ -48,18 +64,19 @@ plot_obo <- function(d, lower.bound=0, upper.bound=100){
 
   xlines <- taxidmap %>%
     dplyr::group_by(.data$ps) %>%
-    dplyr::summarize(n = dplyr::n()) %>%
+    dplyr::summarize(n = length(.data$ps)) %>%
     dplyr::arrange(-.data$ps) %>%
     { Reduce(sum, .$n, accumulate=TRUE) } %>%
     { .[-length(.)] } %>%
     { . + 0.5 } %>%
-    as.data.frame
+    as.data.frame %>%
+    magrittr::set_names('xlines')
 
   stats <- d %>%
     dplyr::select(.data$qseqid, .data$score, .data$evalue, .data$staxid) %>%
     merge(taxidmap, by='staxid') %>%
     dplyr::group_by(.data$qseqid) %>%
-    dplyr::mutate(index=1:dplyr::n()) %>%
+    dplyr::mutate(index=seq_along(.data$qseqid)) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(eval_bins=eval_bins(.data$evalue)) %>%
     {
@@ -72,7 +89,7 @@ plot_obo <- function(d, lower.bound=0, upper.bound=100){
     function(qseqid) {
       plot_one_obo(stats[[qseqid]], qseqid, xlines)
     }
-  )
+  ) %>% magrittr::set_names(stats$qseqid)
 }
 
 make_obo_pdf <- function(d, title='obo.pdf', width=1, height=5, ...){
