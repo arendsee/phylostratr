8453cf97189f4a2105e5f87c87bd60d92f2f8ab1
Zebulun Arendsee <arendsee@iastate.edu>
2017-12-01 16:42:19 -0600

    Add incomplete tree_building test set

---
+++ tests/testthat/test-tree_build.R.R
@@ -0,0 +1,9 @@
+context("tree_building.R")
+
+data(atree)
+prok <- uniprot_sample_prokaryotes()
+
+test_that("lineages_to_phylo", {
+  lin <- taxizedb::classification(c(3702, 9606))
+  expect_equal(tree_names(lineages_to_phylo(lin)), c('Arabidopsis thaliana', 'Homo sapiens', '2759'))
+})
