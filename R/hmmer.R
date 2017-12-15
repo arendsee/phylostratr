f4525c99bf776f89781f01ccd927d56377b5ef4f
Zebulun Arendsee <arendsee@iastate.edu>
2017-12-15 16:08:47 -0600

    Delegate hmmer interface to rhmmer

--- R/hmmer.R
+++ R/hmmer.R
@@ -21,19 +21,11 @@ run_hmmscan <- function(
       db, fastafile
     )
   )
-  domtblout <- hmmer_parse_domtblout(domtblout_file)
-  tblout <- hmmer_parse_tblout(tblout_file)
+  domtblout <- rhmmer::read_domtblout(domtblout_file)
+  tblout <- rhmmer::read_tblout(tblout_file)
   unlink(tblout_file)
   unlink(domtblout_file)
   list(domtblout=domtblout, tblout=tblout)
-}
-
-hmmer_parse_tblout <- function(file){
-  parse_hmmer_output(file, 'tblout')
-}
-
-hmmer_parse_domtblout <- function(file){
-  parse_hmmer_output(file, 'domtblout')
 }
 
 make_hmmscan_filter <- function(by='domain_ievalue', k=1e-5, op='<'){
@@ -78,8 +70,8 @@ strata_load_hmmscan <- function(
   }
   strata@data$hmmscan <- lapply(strata@tree$tip.label, function(taxid){
     list(
-      domtblout = hmmer_parse_domtblout(file.path(dir, paste0(taxid, '.domtblout.tab'))),
-      tblout = hmmer_parse_tblout(file.path(dir, paste0(taxid, '.tblout.tab')))
+      domtblout = rhmmer::read_domtblout(file.path(dir, paste0(taxid, '.domtblout.tab'))),
+      tblout = rhmmer::read_tblout(file.path(dir, paste0(taxid, '.tblout.tab')))
     )
   })
   strata
@@ -115,69 +107,4 @@ strata_retrieve_PFAM_domains <- function(strata, dir='pfam-domains', version='31
     # ----------------------- end catch
     # on failure, return NULL
   })
-}
-
-parse_hmmer_output <- function(file, type){
-  column_names <- if(type == 'tblout'){
-    c(
-      'domain_name',
-      'domain_accession',
-      'query_name',
-      'query_accession',
-      'sequence_evalue',
-      'sequence_score',
-      'sequence_bias',
-      'best_domain_evalue',
-      'best_domain_score',
-      'best_domain_bis',
-      'domain_number_exp',
-      'domain_number_reg',
-      'domain_number_clu',
-      'domain_number_ov',
-      'domain_number_env',
-      'domain_number_dom',
-      'domain_number_rep',
-      'domain_number_inc',
-      'description'
-    )
-  } else if(type == 'domtblout') {
-    c(
-      'domain_name',
-      'domain_accession',
-      'domain_len',
-      'query_name',
-      'query_accession',
-      'qlen',
-      'sequence_evalue',
-      'sequence_score',
-      'sequence_bias',
-      'domain_N',
-      'domain_of',
-      'domain_cevalue',
-      'domain_ievalue',
-      'domain_score',
-      'domain_bias',
-      'hmm_from',
-      'hmm_to',
-      'ali_from',
-      'ali_to',
-      'env_from',
-      'env_to',
-      'acc',
-      'description'
-    )
-  }
-
-  N <- length(column_names)
-
-  readr::read_lines(file) %>%
-    Filter(f=function(x) grepl('^[^#]', x)) %>%
-    sub(
-      pattern = sprintf("(%s) *(.*)", paste0(rep('\\S+', N-1), collapse=" +")),
-      replacement = '\\1\t\\2',
-      perl = TRUE
-    ) %>%
-    paste0(collapse="\n") %>%
-    readr::read_tsv(col_names=c('X', 'description')) %>%
-    tidyr::separate(.data$X, head(column_names, -1), sep=' +')
 }
