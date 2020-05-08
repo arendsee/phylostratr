89f4dedf9187250cf25135cab9416752ca580396
urmi-21 <mrbains21@gmail.com>
2020-05-08 15:40:03 -0500

    add diamond support

--- R/blast.R
+++ R/blast.R
@@ -259,3 +259,108 @@ merge_besthits <- function(strata){
   do.call(what=rbind) %>%
   dplyr::mutate(ps = as.integer(.data$ps))
 }
+
+
+##
+#author UrMi
+#' Blast strata using diamond blastp
+#'
+#' @param strata Strata object where the 'faa' vector is included in the data slot
+#' @param makedb_args Additional arguments passed to \code{make_diamond_database} 
+#' @param blast_args Additional arguments passed to \code{run_diamond_blastp}
+#' @return named list of phylostrata, where each element is a vector of blast result filenames 
+#' @export
+strata_blast <- function(
+  strata,
+  makedb_args=list(),
+  blast_args=list()
+){
+  is_valid_strata(strata, required='faa')
+  
+  query <- strata@data$faa[[strata@focal_species]]
+  
+  strata@data$blast_result <- lapply(names(strata@data$faa), function(taxid){
+    fastafile <- strata@data$faa[[taxid]]
+    blastdb <- do.call(make_diamond_database, args=append(fastafile, makedb_args))
+    blast_args <- append(list(query, taxid, blastdb=blastdb), blast_args)
+    do.call(run_diamond_blastp, args=blast_args)
+  })
+  names(strata@data$blast_result) <- names(strata@data$faa)
+  strata
+}
+
+
+#' Build a diamond database for one species
+#'
+#' @param fastafile The path to a protein FASTA file
+#' @param blastdb The directory in which the diamond database should be stored
+#' @param verbose Print progress messages
+#' @return path to diamond database (without extension)
+make_diamond_database <- function(
+  fastafile,
+  blastdb = 'blastdb',
+  verbose = FALSE
+){
+  out <- file.path(blastdb, basename(fastafile))
+  if(!.blastdb_exists(out)){
+    maybe_message("%s: making diamond database ...", verbose, basename(fastafile))
+    dbmsg <- system2(
+      'diamond makedb',
+      stderr = TRUE,
+      stdout = TRUE,
+      args   = c('--in', fastafile, '-d', out)
+    )
+    out<-paste(out,'.dmnd',sep="")
+    maybe_message(dbmsg, verbose)
+    if(!.blastdb_exists(out)){
+      stop(sprintf("Failed to make diamond database %s", out))
+    }
+  }
+  out
+}
+
+#' BLAST query protein FASTA file against a subject species 
+#'
+#' @param query_fastafile A protein FASTA file for the focal species
+#' @param subject_taxid The subject NCBI taxon ID
+#' @param blastdb A path to a diamond database (as returned from \code{make_blast_database})
+#' @param blastresult The output TAB-delimited result file
+#' @param nthreads Number of threads
+#' @param seg Whether to mask the query protein sequences
+#' @param verbose Print progress messages
+#' @return The path to the tabular BLAST result output 
+run_diamond_blastp <- function(
+  query_fastafile,
+  subject_taxid,
+  blastdb,
+  blastresult  = paste0(subject_taxid, ".tab"),
+  nthreads     = 1,
+  seg          = FALSE,
+  verbose      = TRUE
+){
+  if(file.exists(blastresult)){
+    maybe_message("Skipping %s", verbose, subject_taxid)
+  } else {
+    maybe_message("%s: Diamond blasting ...", verbose, subject_taxid)
+    system2(
+      'diamond blastp',
+      stdout=blastresult,
+      args=c(
+        '-d', blastdb,
+        '-q', query_fastafile,
+        '--outfmt', '"6 qseqid sseqid qstart qend sstart send evalue score"',
+        '-p', nthreads,
+        '--masking', if(seg) {'1'} else {'0'}
+      )
+    )
+    # Add the subject taxon ID, name and order columns, write with header
+    read_blast(blastresult, with_taxid=FALSE, col_names=FALSE) %>%
+      dplyr::mutate(staxid = as.character(subject_taxid)) %>%
+      readr::write_tsv(path=blastresult)
+  }
+  blastresult
+}
+
+
+
+
